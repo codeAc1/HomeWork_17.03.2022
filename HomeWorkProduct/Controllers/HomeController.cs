7654696de40f668219626e112bda9f965171277f
Sakhavat <sakhavatra@code.edu.az>
2022-03-17 11:28:58 +0400

    first commit

---
+++ HomeWorkProduct/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using HomeWorkProduct.DAL;
+using HomeWorkProduct.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWorkProduct.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly WorkDbContext _context;
+        public HomeController(WorkDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            
+            List<Product> products = _context.Products.ToList();
+
+            return View();
+        }
+    }
+}
---
+++ HomeWorkProduct/DAL/WorkDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HomeWorkProduct.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWorkProduct.DAL
+{
+    public class WorkDbContext:DbContext
+    {
+        public WorkDbContext(DbContextOptions<WorkDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
